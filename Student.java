0f8b27658c1d73d822c195cde8240766ebc8f1bd
Esra Cennet Güver <61980724+esracennetguver@users.noreply.github.com>
2021-07-12 12:23:53 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+public class Student extends User{
+
+    private String number;
+    private String advisor;
+
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(String advisor) {
+        this.advisor = advisor;
+    }
+
+
+
+}
