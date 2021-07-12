49d219af0b2d7ed9db63f59a9a984f14d86c948e
Esra Cennet Güver <61980724+esracennetguver@users.noreply.github.com>
2021-07-12 12:24:22 +0300

    Create Instructor.java

---
+++ Instructor.java
@@ -0,0 +1,14 @@
+public class Instructor extends User{
+
+    private int pay;
+    private int seniorityYear;
+
+    public int getPay() {
+        return pay;
+    }
+
+    public int getSeniorityYear() {
+        return seniorityYear;
+    }
+
+}
